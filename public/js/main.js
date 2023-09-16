3fb88864de211296efd5dc2112da9264838e2599
Ernest Paśnik <hajtusgn@gmail.com>
2023-09-16 16:10:05 +0200

    Optimization

--- public/js/main.js
+++ public/js/main.js
@@ -11,15 +11,23 @@ function incNbrRec(e, n, c) {
   }, 10))
 }
 
+function loadCSSAsync(href) {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = href;
+  document.head.appendChild(link);
+}
+
+loadCSSAsync("https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap");
+
 window.onload = (event) => {
   incEltNbr('servers')
   incEltNbr('players')
-  let el = document.querySelector('body > div > div.serverlist > table > thead > tr > th:nth-child(5)')
+  let el = document.querySelector('body > div > div.serverlist > table > thead > tr > th:nth-child(4)')
   if (el) {
     el.click()
     el.click()
   }
-
   el = document.querySelector("body > div > div > div > div > div:nth-child(2) > table > thead > tr > th:nth-child(3)")
   if (el) {
     el.click()

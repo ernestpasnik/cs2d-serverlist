65a6abc4dd5758f6e49ddefea68dba81ad8fdef4
Ernest Paśnik <erpa@gmx.com>
2025-05-11 20:07:35 +0200

    Add external resources

--- src/maps/maps.js
+++ src/maps/maps.js
@@ -4,6 +4,7 @@ const crypto = require('crypto')
 const Parser = require('./parser')
 const Minimap = require('./minimap')
 const redis = require('../utils/redis')
+const { bytesToSize } = require('../utils/utils')
 
 const maplist = []
 
@@ -13,6 +14,29 @@ const generateAndStoreMinimap = async (mapName, mapPath) => {
     const parsed = new Parser(buffer).parse()
     parsed.minimap = await new Minimap().generate(parsed)
     parsed.sha256 = crypto.createHash('sha256').update(buffer).digest('hex')
+    parsed.resources = []
+
+    parsed.entities.forEach(v => {
+      if (v.type == 11) {
+        parsed.nobuying = true
+      } else if (v.type == 12) {
+        parsed.noweapons = true
+      } else if ([22, 23, 28].includes(v.type)) {
+        const path = v.settings.string[0].replace(/\\/g, '/')
+        if (!path) return
+
+        const exists = parsed.resources.some(r => r.path === path)
+        if (exists) return
+
+        const p = `${process.env.CS2D_DIRECTORY}/${path}`
+        const bytes = fs.existsSync(p) ? fs.statSync(p).size : 0
+        const size = bytesToSize(bytes)
+        parsed.resources.push({ path, bytes, size })
+      } else if (v.type == 70) {
+        parsed.teleporters = true
+      }
+    })
+    parsed.resources.sort((a, b) => a.bytes - b.bytes)
     await redis.set(`map:${mapName}`, JSON.stringify(parsed))
   } catch (err) {
     console.error(mapName, err)
@@ -31,7 +55,7 @@ const generateMinimapsForAllMaps = async (directory) => {
       maplist.push(mapName)
 
       // Skip generation if the map data is already cached in Redis
-      if (await redis.exists(`map:${mapName}`)) continue
+      //if (await redis.exists(`map:${mapName}`)) continue
       await generateAndStoreMinimap(mapName, mapPath)
       i++
     }

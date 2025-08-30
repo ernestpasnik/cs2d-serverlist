c26b720382de06914a5d57ec7560e2524328ce85
Ernest Paśnik <erpa@gmx.com>
2025-08-30 04:40:28 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,19 @@ module.exports = {
     {
       name: 'cs2d-serverlist',
       script: 'app.js',
-      autorestart: true
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      error_file: './logs/error.log',
+      out_file: './logs/out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss'
     }
   ]
 };
